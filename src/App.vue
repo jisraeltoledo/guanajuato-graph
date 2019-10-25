73edc6825824cdf4631bceb5a4800e0936b22b7e
Israel Toledo <israel@omr.mx>
2019-10-25 12:20:39 -0500

    separando espacios

--- src/App.vue
+++ src/App.vue
@@ -14,40 +14,46 @@
     <v-content>
       <v-layout row>
         <v-flex xs4 ma-5>
-          <v-text-field 
+          <v-select :items="items" v-model="net"></v-select>
+        </v-flex>
+        <v-flex xs4 ma-5>
+          <v-text-field
             v-model="query"
             name="input-2"
             label="Nodo a Buscar"
             value="Input text"
             class="input-group--focused"
           ></v-text-field>
-          
         </v-flex>
-        <v-flex xs4 ma-5>
+        <v-flex xs2 ma-5>
           <v-btn color="info" @click="search">Buscar</v-btn>
         </v-flex>
       </v-layout>
       <v-layout row>
-        
         <v-flex xs5 ma-5>
           <v-subheader>Targets</v-subheader>
-          <v-text-field 
-          v-model="asSource" 
-          id="testing" 
-          name="input-1" 
-          label="Search result: Targets"></v-text-field>
+          <v-text-field
+            v-model="asSource"
+            id="testing"
+            name="input-1"
+            label="Search result: Targets"
+          ></v-text-field>
         </v-flex>
         <v-flex xs5 ma-5>
           <v-subheader>Sources</v-subheader>
-          <v-text-field 
-          v-model="asTarget" 
-          id="testing" 
-          name="input-1" 
-          label="Search Results: Sources"></v-text-field>
+          <v-text-field
+            v-model="asTarget"
+            id="testing"
+            name="input-1"
+            label="Search Results: Sources"
+          ></v-text-field>
         </v-flex>
-        </v-layout>
-      <cytoscape ref="cy" :config="config" v-on:zoom="getZoom">
-        <cy-element v-for="def in elements" :key="`${def.data.id}`" :definition="def" />
+      </v-layout>
+      <cytoscape ref="cy1" :config="config" v-on:zoom="getZoom" v-show="net=='sitc2'">
+        <cy-element v-for="def in elements1" :key="`${def.data.id}`" :definition="def" />
+      </cytoscape>
+      <cytoscape ref="cy2" :config="config" v-on:zoom="getZoom" v-show="net=='hs92'">
+        <cy-element v-for="def in elements2" :key="`${def.data.id}`" :definition="def" />
       </cytoscape>
     </v-content>
   </v-app>
@@ -63,65 +69,14 @@ export default {
   components: {
     //HelloWorld
   },
-  watch: {},
-  methods: {
-    getZoom() {
-      // this.$refs.cy.cy.then(c => {
-        
-      // });
-    },
-    afterCreated(cy) {
-      this.cy = cy;
-    },
-    search() {
-      
-      this.asSource = network1.edges.filter(el => {
-        return el.source == this.query;
-      }).map(a => a.target);
-      
-      this.asTarget = network1.edges.filter(el => {
-        return el.target == this.query;
-      }).map(a => a.source);;
-      
-      this.asSource2 = network2.edges.filter(el => {
-        return el.source == this.query;
-      }).map(a => a.target);
-      
-      this.asTarget2 = network2.edges.filter(el => {
-        return el.target == this.query;
-      }).map(a => a.source);
-      
-
-      this.asSource = this.asSource.concat (this.asSource2);
-      this.asTarget = this.asTarget.concat (this.asTarget2);
-    },
-    fillElements (network, number){
-      network.nodes.forEach(node => {
-      this.elements.push({
-        data: {
-          id: node.id +"_"+ number
-        },
-        position: {
-          x: node.x,
-          y: node.y
-        }
-      });
-    });
-    
-    network.edges.forEach(edge => {
-      let e = {
-        data: {
-          id: "e_" + ++this.i,
-          source: edge.source+"_"+ number,
-          target: edge.target+"_"+ number,
-          strength: edge.strength
-        }
-      };
-      this.elements.push(e);
-    });
+  watch: {
+    net(nv) {
+      console.log(nv);
     }
   },
   data: () => ({
+    net: "sitc2",
+    items: ["hs92", "sitc2"],
     i: 0,
     query: "",
     asSource: null,
@@ -154,19 +109,84 @@ export default {
       zoom: 0.2,
       pan: { x: 100, y: -300 }
     },
-    elements: []
+    elements1: [],
+    elements2: []
   }),
+  methods: {
+    getZoom() {
+      // this.$refs.cy.cy.then(c => {
+      // });
+    },
+    search() {
+      if (this.net === "sitc2") {
+        this.searchOn(network1);
+      } else {
+        this.searchOn(network2);
+      }
+    },
+    searchOn(network) {
+      this.asSource = network.edges
+        .filter(el => {
+          return el.source == this.query;
+        })
+        .map(a => a.target);
 
+      this.asTarget = network.edges
+        .filter(el => {
+          return el.target == this.query;
+        })
+        .map(a => a.source);
+    },
+    fillElements(network, elements) {
+      network.nodes.forEach(node => {
+        elements.push({
+          data: {
+            id: node.id
+          },
+          position: {
+            x: node.x,
+            y: node.y
+          }
+        });
+      });
+
+      network.edges.forEach(edge => {
+        let e = {
+          data: {
+            id: "e_" + ++this.i,
+            source: edge.source,
+            target: edge.target,
+            strength: edge.strength
+          }
+        };
+        elements.push(e);
+      });
+    },
+    fit() {
+      console.log("fit", this.$refs);
+      if (this.$refs.cy1) {
+        this.$refs.cy1.cy.then(c => {
+          c.container().style.height = "1000px";
+          c.fit();
+        });
+      }
+
+      if (this.$refs.cy2) {
+        this.$refs.cy2.cy.then(c => {
+          c.container().style.height = "1000px";
+          c.fit();
+        });
+      }
+    }
+  },
   mounted() {
-    //this.cy.fit ();
-    this.$refs.cy.cy.then(c => {
-      c.container().style.height = "1000px";
-      c.fit();
-    });
+    this.fit();
+    console.log(this.elements1);
+    console.log(this.elements2);
   },
   created() {
-    this.fillElements (network1, "1");
-    this.fillElements (network2, "2");
+    this.fillElements(network1, this.elements1);
+    this.fillElements(network2, this.elements2);
   }
 };
 </script>
